710d38398032d53508cfc9600c120286a5eaf2a9
Misha <makovkamisha10@gmail.com>
2025-05-31 18:40:44 +0300

    Added index for ECO

--- query_versions/step2_indexing.sql
+++ query_versions/step2_indexing.sql
@@ -2,5 +2,6 @@ use tournament_chess_games;
 
 create index elo_result_white_index on games (WhiteElo, Result, White);
 create index white_index on games (White);
+create index eco_index on games (ECO);
 
 show indexes from games;
