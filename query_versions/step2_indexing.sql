7d0e093976220b999711e479f5f780f66973cb90
Misha <makovkamisha10@gmail.com>
2025-05-31 23:36:46 +0300

    Updated indexes

--- query_versions/step2_indexing.sql
+++ query_versions/step2_indexing.sql
@@ -1,7 +1,6 @@
 use tournament_chess_games;
 
 create index elo_result_white_index on games (WhiteElo, Result, White);
-create index white_index on games (White);
-create index eco_index on games (ECO);
+create index white_eco_black_index on games (White, ECO, Black);
 
 show indexes from games;
